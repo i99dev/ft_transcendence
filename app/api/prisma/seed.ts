e327193fbe5f1c3272b97747d2b61ec3276186c9
Bassam <bassam1881999@gmail.com>
2023-01-27 19:49:44 +0400

    Update User table, add Friends table

--- app/api/prisma/seed.ts
+++ app/api/prisma/seed.ts
@@ -8,28 +8,59 @@ const prisma = new PrismaClient();
 async function main() {
   // create two dummy users
   const user1 = await prisma.user.upsert({
-    where: { username: 'bnaji' },
+    where: { login: 'bnaji' },
     update: {},
     create: {
-      username: 'bnaji',
-      fullname: 'Bassam Naji',
+      login: 'bnaji',
       email: 'bnaji@student.42abudhabi.ae',
-      avatar: 'https://cdn.intra.42.fr/users/9dd4ce5214846a4cf919a6290e7db56c/bnaji.jpg'
+      status: 'ONLINE',
+      first_name: 'Bassam',
+      last_name: 'Naji',
+      image: 'https://cdn.intra.42.fr/users/9dd4ce5214846a4cf919a6290e7db56c/bnaji.jpg'
     },
   });
 
   const user2 = await prisma.user.upsert({
-    where: { username: 'isaad' },
+    where: { login: 'isaad' },
     update: {},
     create: {
-      username: 'isaad',
-      fullname: 'Imad Saad',
+      login: 'isaad',
       email: 'isaad@student.42abudhabi.ae',
-      avatar: 'https://cdn.intra.42.fr/users/f63f7f3080ae66de20d2b71c03559aaf/isaad.jpg'
+      first_name: 'Imad',
+      last_name: 'Saad',
+      status: 'LIVE',
+      image: 'https://cdn.intra.42.fr/users/f63f7f3080ae66de20d2b71c03559aaf/isaad.jpg'
     },
   });
 
-  console.log({ user1, user2 });
+  const friend1 = await prisma.user.upsert({
+    where: { login: 'friend1' },
+    update: {
+      login: 'friend3'
+    },
+    create: {
+      login: "friend1",
+      email: "friend1@email.com",
+      status: "ONLINE",
+      first_name: "Jane",
+      last_name: "Smith",
+    }
+  })
+
+  const friend2 = await prisma.user.upsert({
+    where: { login: 'friend2' },
+    update: {
+    },
+    create: {
+      login: "friend2",
+      email: "friend1@email.com",
+      status: "ONLINE",
+      first_name: "Jane",
+      last_name: "Smith",
+    }
+  })
+
+  console.log({ user1, user2, friend1, friend2 });
 }
 
 // execute the main function
