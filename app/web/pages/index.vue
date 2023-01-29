67176c58e859da6f20658f29251b8d21d6c24d32
Saxsori <jungxbra@gmail.com>
2023-01-29 07:30:29 +0400

    fix the button click issue

--- app/web/pages/index.vue
+++ app/web/pages/index.vue
@@ -9,21 +9,21 @@
 			<div class="mb-8 flex flex-col items-center">
 			
 				<img src="https://cdn3.iconfinder.com/data/icons/one-piece-colored/48/Cartoons__Anime_One_Piece_Artboard_6-1024.png" width="150" alt="" srcset="" />
-				
 				<h1 class="mb-2 text-2xl">Ping Pong</h1>
 				<!-- <span class="text-gray-300">Enter Login Details</span> -->
 			
 			</div>
 
 			<form action="#">
-				<div class="mt-8 flex justify-center text-lg text-black">		
-				
-					<button type="submit" class="rounded-3xl bg-gray-90 bg-opacity-50 px-20 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">
-						<img src="https://signin.intra.42.fr/assets/42_logo-7dfc9110a5319a308863b96bda33cea995046d1731cebb735e41b16255106c12.svg" width="35" height="20" alt="icon">
-						<NuxtLink to="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-0790e6eeae1028551e49d7958f62e9e0194a3816c15060dfedd2064583678acb&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin_new_user&response_type=code"></NuxtLink>
-					</button>
-				
+                
+				<div class="mt-8 flex justify-center text-lg text-black">
+                  
+					<div class="rounded-3xl bg-gray-90 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">
+                         <img src="https://signin.intra.42.fr/assets/42_logo-7dfc9110a5319a308863b96bda33cea995046d1731cebb735e41b16255106c12.svg" v-bind:href="link" v-on:click="go" width="35" height="20" alt="icon">
+					</div>
+                
 				</div>
+            
 			</form>
 	
 		</div>
@@ -38,6 +38,7 @@
 export default {
 	data() {
 		return {
+			link: "https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-0790e6eeae1028551e49d7958f62e9e0194a3816c15060dfedd2064583678acb&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin_new_user&response_type=code",
 			user: {
 				email: '',
 				password: '',
@@ -45,6 +46,9 @@ export default {
 		};
 	},
 	methods: {
+		go() {
+			window.location.href = this.link;
+		},
 		logData() {
 			console.log(this.user.email);
 			console.log(this.user.password);
