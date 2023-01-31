4004088ccb0900015a12c1e897cc148ce6a2c4f6
Saxsori <jungxbra@gmail.com>
2023-01-31 20:00:35 +0400

    the middleware is simply applied

--- app/web/nuxt.config.ts
+++ app/web/nuxt.config.ts
@@ -1,4 +1,11 @@
+import { useRuntimeConfig } from "#app";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss'],
+	runtimeConfig: {
+		public: {
+		  API_KEY: process.env.API_KEY,
+		}
+	  },
 })
