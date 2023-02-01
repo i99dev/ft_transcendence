62a9c7c0ff3016a4f3e448b35693b99c4a705073
i99dev <obaid.altuniji@gmail.com>
2023-02-01 11:57:02 +0400

    feat:add server for mockup #1

--- app/web/nuxt.config.ts
+++ app/web/nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      API_KEY: process.env.API_KEY,
+      BACKEND_PORT: process.env.BACKEND_PORT,
+    },
+  },
   modules: ['@nuxtjs/tailwindcss'],
 })
--- app/web/pages/index.vue
+++ app/web/pages/index.vue
@@ -1,62 +1,72 @@
 <template>
+  <div
+    class="flex h-screen w-full items-center justify-center bg-gray-900 bg-no-repeat"
+    style="
+      background-image: url('https://d1an7elaqzcblb.cloudfront.net/RSMIG/PROD/eukddc/PACKSHOTS/d5f7084f485e1f652784217487fe278c.jpeg');
+      background-size: 100% 100%;
+    "
+  >
+    <div
+      class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8"
+    >
+      <div class="text-white">
+        <div class="mb-8 flex flex-col items-center">
+          <img
+            src="https://cdn3.iconfinder.com/data/icons/one-piece-colored/48/Cartoons__Anime_One_Piece_Artboard_6-1024.png"
+            width="150"
+            alt=""
+            srcset=""
+          />
+          <h1 class="mb-2 text-2xl">Ping Pong</h1>
+          <!-- <span class="text-gray-300">Enter Login Details</span> -->
+        </div>
 
-<div class="flex h-screen w-full items-center justify-center bg-gray-900 bg-no-repeat" style="background-image:url('https://d1an7elaqzcblb.cloudfront.net/RSMIG/PROD/eukddc/PACKSHOTS/d5f7084f485e1f652784217487fe278c.jpeg'); background-size: 100% 100%; ">
-		
-	<div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
-	
-		<div class="text-white">
-			
-			<div class="mb-8 flex flex-col items-center">
-			
-				<img src="https://cdn3.iconfinder.com/data/icons/one-piece-colored/48/Cartoons__Anime_One_Piece_Artboard_6-1024.png" width="150" alt="" srcset="" />
-				<h1 class="mb-2 text-2xl">Ping Pong</h1>
-				<!-- <span class="text-gray-300">Enter Login Details</span> -->
-			
-			</div>
-
-			<form action="#">
-                
-				<div class="mt-8 flex justify-center text-lg text-black">
-                  
-					<div class="rounded-3xl bg-gray-90 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600">
-                         <img src="https://signin.intra.42.fr/assets/42_logo-7dfc9110a5319a308863b96bda33cea995046d1731cebb735e41b16255106c12.svg" v-bind:href="link" v-on:click="go" width="35" height="20" alt="icon">
-					</div>
-                
-				</div>
-            
-			</form>
-	
-		</div>
-	
-	</div>
-
-</div>
-
+        <form action="#">
+          <div class="mt-8 flex justify-center text-lg text-black">
+            <div
+              class="rounded-3xl bg-gray-90 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors duration-300 hover:bg-yellow-600"
+            >
+              <img
+                src="https://signin.intra.42.fr/assets/42_logo-7dfc9110a5319a308863b96bda33cea995046d1731cebb735e41b16255106c12.svg"
+                v-bind:href="link"
+                v-on:click="go"
+                width="35"
+                height="20"
+                alt="icon"
+              />
+            </div>
+          </div>
+        </form>
+        {{ data }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 export default {
-	data() {
-		return {
-			link:  this.$nuxt.$config.public.API_KEY,
-			user: {
-				email: '',
-				password: '',
-			},
-		};
-	},
-	methods: {
-		go() {
-			window.location.href = this.link;
-		},
-		logData() {
-			console.log(this.user.email);
-			console.log(this.user.password);
-		},
-	},
-};
-
+  data() {
+    //fetch
+    const { data, pending, error, refresh } = useFetch(
+      `http://localhost:${this.$nuxt.$config.public.BACKEND_PORT}/api/users`
+    )
+    return {
+      link: this.$nuxt.$config.public.API_KEY,
+      data,
+      user: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+  methods: {
+    go() {
+      window.location.href = this.link
+    },
+    logData() {
+      console.log(this.user.email)
+      console.log(this.user.password)
+    },
+  },
+}
 </script>
-
-
