b342311d8471a2bfcab01135667154f8fae43991
Imad Saad <em4d2002@gmail.com>
2023-02-03 19:13:40 +0400

    user repo edited

--- app/api/src/app/user/repository/user.repository.ts
+++ app/api/src/app/user/repository/user.repository.ts
@@ -3,12 +3,10 @@ import { PrismaClient, User } from '@prisma/client';
 export class UserRepository {
   prisma = new PrismaClient();
 
-  async sortUserById() {
+	async SortMany(orderBy: object){
 		const sortedUsers = await this.prisma.user.groupBy({
-			by: ['token', 'id', 'login', 'first_name', 'last_name', 'image', 'email', 'total_wins', 'total_loses', 'exp_level', 'points', 'created_at', 'last_login', 'status'],
-			orderBy: {
-				id: 'asc'
-			}
+			by: ['id', 'login', 'first_name', 'last_name', 'image', 'email', 'total_wins', 'total_loses', 'exp_level', 'points', 'created_at', 'last_login', 'status'],
+			orderBy: orderBy
 		});
 		return sortedUsers;
 	}
