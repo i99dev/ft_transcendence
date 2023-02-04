a44bff03de78772e12c8f7fa36f407868534c558
Imad Saad <em4d2002@gmail.com>
2023-02-04 15:21:52 +0400

    e2e tests for user endpoints are done

---
+++ app/api/test/e2e/api/user/user.e2e-spec.ts
@@ -0,0 +1,62 @@
+import { UserPatchDto } from './../../../../src/app/user/dto/user.dto';
+import { IsNotEmpty } from 'class-validator';
+import { UserModule } from './../../../../src/app/user/user.module';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Body, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../../src/app.module';
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, UserModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/api/users (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/users')
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.IsNotEmpty);
+      });
+  });
+
+  it('/api/users/:name (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/api/users/isaad')
+      .expect(200)
+      .expect((res)=>{
+          expect(res.body.IsNotEmpty);
+        });
+    });
+    
+    it('/api/users (GET)', () => {
+    return request(app.getHttpServer())
+    .get('/api/users?sort=total_wins&order=desc')
+    .expect(200)
+    .expect((res)=>{
+        expect(res.body.IsNotEmpty);
+    });
+});
+
+const data: any = {
+    first_name: 'Imad',
+    last_name: 'Saad'
+  };
+
+  it('/api/users/:name (Patch)', () => {
+    return request(app.getHttpServer())
+      .patch('/api/users/isaad', data )
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.IsNotEmpty);
+      });
+  });
+
+});
