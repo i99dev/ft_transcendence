4b5f7972a02dea63bfea41cfadc2dd7b57fc04b6
bassamnaji <bassam1881999@gmail.com>
2023-02-08 20:10:31 +0400

    remove modules directory that got created from merge

--- app/api/src/modules/achievement/achievement.controller.ts
+++
@@ -1,7 +0,0 @@
-import { AchievementService } from './achievement.service';
-import { Controller } from '@nestjs/common';
-
-@Controller('/api/users')
-export class AchievementController {
-  constructor(private readonly AchievementService: AchievementService) {}
-}
--- app/api/src/modules/friend/friend.controller.ts
+++
@@ -1,7 +0,0 @@
-import { FriendService } from './friend.service';
-import { Controller } from '@nestjs/common';
-
-@Controller('/api/users')
-export class FriendController {
-  constructor(private readonly FriendService: FriendService) {}
-}
