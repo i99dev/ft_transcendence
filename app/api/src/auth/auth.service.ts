40be343e27e071a63ced0d1a7da6bc156048b1ab
bassamnaji <bassam1881999@gmail.com>
2023-02-11 22:55:21 +0400

    add auth e2e and unit tests

--- app/api/src/auth/auth.service.ts
+++ app/api/src/auth/auth.service.ts
@@ -1,22 +1,20 @@
 import { UserGetDto } from '../module/user/dto/user.dto';
-import axios from 'axios';
-import { Injectable, HttpStatus, UnauthorizedException } from '@nestjs/common';
-import { intraConstants } from '../common/constants/setting';
+import { Injectable, HttpStatus } from '@nestjs/common';
 import { IntraAccessToken, Me } from './interfaces/intra.interface';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../module/user/user.service';
-import { ConfigService } from '@nestjs/config';
+import { AuthRepository } from './repository/auth.repository';
 
 @Injectable({})
 export class AuthService {
   constructor(
     private jwtService: JwtService,
     private userService: UserService,
-    private configService: ConfigService,
+    private authrepository: AuthRepository,
   ) {}
 
   async checkUserAccountOnDb(
-    intraUser: Me,
+    intraUser,
   ): Promise<{ httpStatus: HttpStatus; user: UserGetDto }> {
     const myuser: UserGetDto = await this.userService.getUser(intraUser.login);
     if (!myuser) {
@@ -29,38 +27,9 @@ export class AuthService {
   }
 
   async validateUserWithIntra(code: string): Promise<Me> {
-    const intraToken: IntraAccessToken = await this.getIntraAccessToken(code);
-    return await this.getUserIntraProfile(intraToken);
-  }
-
-  async getIntraAccessToken(authCode: string): Promise<IntraAccessToken> {
-    try {
-      return (
-        await axios.post(intraConstants.paths.token, {
-          grant_type: intraConstants.grant_type,
-          client_id: this.configService.getOrThrow<string>('auth.clientId'),
-          client_secret:
-            this.configService.getOrThrow<string>('auth.clientSecret'),
-          code: authCode,
-          redirect_uri:
-            this.configService.getOrThrow<string>('auth.redirectUri'),
-        })
-      ).data.access_token;
-    } catch (error) {
-      throw new UnauthorizedException('Invalid Authorization Code');
-    }
-  }
-
-  async getUserIntraProfile(accessToken: IntraAccessToken): Promise<Me> {
-    try {
-      return (
-        await axios.get(intraConstants.paths.me, {
-          headers: { Authorization: `Bearer ${accessToken}` },
-        })
-      ).data;
-    } catch (error) {
-      throw new UnauthorizedException('Invalid Intra User');
-    }
+    const intraToken: IntraAccessToken =
+      await this.authrepository.getIntraAccessToken(code);
+    return await this.authrepository.getUserIntraProfile(intraToken);
   }
 
   async getJwt(user: UserGetDto): Promise<string> {
---
+++ app/api/test/e2e/auth/auth.e2e-spec.ts
@@ -0,0 +1,72 @@
+import { AuthModule } from '../../../src/auth/auth.module';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Body, INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from '../../../src/app.module';
+import { AuthGuard } from '@nestjs/passport';
+import { FtAuthGuard } from '../../../src/common/guards/ft.auth.gaurd';
+import { FtStrategy } from '../../../src/auth/strategy/ft.strategy';
+import { AuthService } from '../../../src/auth/auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+  let authService: AuthService;
+  let configService: ConfigService;
+  let jwtService: JwtService;
+  const intraUser = {
+    login: 'bnaji',
+    first_name: 'bassam',
+    last_name: 'naji',
+    email: 'bnaji@student.42abudhabi.ae',
+    image: {
+      link: 'https://cdn.intra.42.fr/users/9dd4ce5214846a4cf919a6290e7db56c/bnaji.jpg',
+    },
+  };
+  var data = JSON.stringify({
+    code: 'auth_code',
+  });
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AuthModule],
+    })
+      .overrideGuard(FtAuthGuard)
+      .useValue({
+        canActivate: jest.fn().mockImplementation((req) => {
+          req.user = intraUser;
+          return req.user;
+        }),
+      })
+      .compile();
+
+    const userMiddleware = (req, res, next) => {
+      req.user = intraUser;
+      next();
+    };
+
+    app = moduleFixture.createNestApplication();
+    app.use(userMiddleware);
+    authService = moduleFixture.get<AuthService>(AuthService);
+    configService = moduleFixture.get<ConfigService>(ConfigService);
+    jwtService = moduleFixture.get<JwtService>(JwtService);
+    await app.init();
+  });
+
+  describe('/auth', () => {
+    it('check if returns acess token', () => {
+      return request(app.getHttpServer())
+        .post('/auth')
+        .send(data)
+        .expect((res) => {
+          expect(res.body.access_token).toBeDefined();
+          const decoded = jwtService.verify(res.body.access_token, {
+            secret: configService.get<string>('JWT_SECRET', 'jwt_secret'),
+          });
+
+          expect(decoded.login).toBe(intraUser.login);
+        });
+    });
+  });
+});
