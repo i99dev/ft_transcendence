e9a466b160c9817dc49a1170003a541078500d94
bassamnaji <bassam1881999@gmail.com>
2023-02-12 17:16:09 +0400

    add global performance interceptor

--- app/api/src/common/interceptors/logging.interceptors.ts
+++ app/api/src/common/interceptors/logging.interceptors.ts
@@ -10,10 +10,9 @@ import { tap } from 'rxjs/operators';
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log(`Before...`);
     const now = Date.now();
     return next
       .handle()
-      .pipe(tap(() => console.log(`After... ${Date.now() - now}ms`)));
+      .pipe(tap(() => console.log(`\x1b[35mPerformance\x1b[0m => \x1b[33m${Date.now() - now}\x1b[90mms\x1b[0m`)));
   }
 }
--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalInterceptors(new LoggingInterceptor())
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('app.port', 8000));
 }
