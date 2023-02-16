cc1991a45e8fc12490fd9575e98a34094a67bb61
Saxsori <jungxbra@gmail.com>
2023-02-17 01:58:17 +0400

    a bit of decoration:)

---
+++ app/web/error.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<NuxtLayout>
+			<main class="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+				<div class="text-center">
+					<p class="text-base font-semibold text-blue-900">404</p>
+					<h1 class="mt-6 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1> 
+				  <div class="mt-3 flex items-center justify-center gap-x-6">
+					  <img src="https://media.tenor.com/yyi93c8CMJkAAAAC/one-piece-chopper.gif">
+					</div>
+				<p class="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
+				  <div class="mt-3 flex items-center justify-center gap-x-6">
+					<a href="/" class="rounded-md bg-blue-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Go back home</a>
+				  </div>
+				</div>
+			  </main>
+			</NuxtLayout>
+	</div>
+</template>
+
