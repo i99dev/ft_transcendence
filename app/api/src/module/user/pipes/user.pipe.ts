89732e099cb4b0eaa589608f46217dcac23dee2c
Imad Saad <em4d2002@gmail.com>
2023-02-19 10:13:16 +0400

    pipe fixed

--- app/api/src/module/user/pipes/user.pipe.ts
+++ app/api/src/module/user/pipes/user.pipe.ts
@@ -1,19 +1,33 @@
+import { UserPatchDto } from './../dto/user.dto';
 import { PipeTransform, Injectable, BadRequestException } from '@nestjs/common'
 import { validate } from 'class-validator'
 import { UserValidPatchDto } from '../dto/user.dto'
 
 @Injectable()
 export class UserPatchValidationPipe implements PipeTransform<any> {
+
+    createAssignValue(): UserPatchDto {
+        const vari: UserPatchDto = {
+            username: 'string',
+            first_name: 'string',
+            last_name: 'string',
+            image: 'string',
+            status: 'ONLINE',
+            two_fac_auth: true,
+            total_loses: 0,
+            total_wins: 0,
+        }
+        return vari;
+    }
+    
     async transform(value: any) {
         const userPatch = new UserValidPatchDto()
-        Object.assign(userPatch, value)
+        const vari = this.createAssignValue();
+        Object.assign(userPatch, vari)
         const userPatchKeys = Object.keys(userPatch)
         const valueKeys = Object.keys(value)
         for (let i = 0; i < valueKeys.length; i++) {
             const key = valueKeys[i]
-            if (!isNaN(parseFloat(key))) {
-                break
-            }
             if (!userPatchKeys.includes(key)) {
                 throw new BadRequestException(`Invalid field: ${key}`)
             }
