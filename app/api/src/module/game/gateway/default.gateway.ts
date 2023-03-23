836b8906e25d1780186bdb49b4ac2cb97b3704e1
Saxsori <jungxbra@gmail.com>
2023-03-23 12:33:53 +0400

    one issue to fix, repeated data

--- app/api/src/module/game/gateway/default.gateway.ts
+++ app/api/src/module/game/gateway/default.gateway.ts
@@ -11,7 +11,9 @@ import {
 import { Server, Socket } from 'socket.io'
 import { DefaultService } from './default.service'
 import { gameResult } from '../actions/endGame'
-
+import { PrismaService } from '@providers/prisma/prisma.service'
+import { MatchHistory, User } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 @WebSocketGateway({
     namespace: '/games',
     cors: { origin: 'http://localhost/play', credentials: true },
@@ -23,9 +25,58 @@ export class DefaultGateway implements OnGatewayConnection, OnGatewayDisconnect 
 
     private logger = new Logger('DefaultGateway')
     private gameResult: gameResult
+    // private prisma: PrismaService
+    private prisma = new PrismaClient()
+    private user: User
 
+	// await this.prisma.user.update({
+	//     where: { login: 'aaljaber' },
+	//     data: {
+	// 		friends: {
+	// 			connect: [{ login: 'bnaji' }, { login: 'isaad' }],
+	// 		},
+	// 	},
+	// })
+	// await this.prisma.user.upsert({
+	// 	where: { login: 'aaljaber'},
+	// 	update: {
+	// 		total_wins: 15,
+	// 	},
+	// 	create: {
+	// 		login: 'aaljaber',
+	// 		username: 'aaljaber',
+	// 		email: 'ss',
+	// 		total_loses: 0,
+	// 	},
+	// });
     constructor(private defaultService: DefaultService, private jwtService: JwtService) {}
-
+	public async createMatchHistory(): Promise<MatchHistory> {
+		const matchHistoryCreate = await this.prisma.matchHistory.create({
+			data: {
+			  opponent: {
+				connect: {
+				  id: 3,
+				},
+			  },
+			  isWinner: true,
+			  at: new Date(),
+			  my_score: 10,
+			  op_score: 1,
+			},
+		  });
+		  return matchHistoryCreate;
+	}
+    async addHistory(): Promise<void> {
+		const matchHistoryCreate = await this.createMatchHistory()
+		await this.prisma.user.update ({
+			where: { login: 'mal-guna'},
+			data: {
+				match_history: { 
+					connect: { id: matchHistoryCreate.id }
+				},
+			},
+		});
+    }
     handleConnection(client: Socket, ...args: any[]) {
         // let token = client.request.headers.authorization
         // this.logger.log(token)
@@ -40,9 +91,10 @@ export class DefaultGateway implements OnGatewayConnection, OnGatewayDisconnect 
         this.logger.log(`Client disconnected: ${client.id}`)
     }
     @SubscribeMessage('leave')
-    leaveGame(client: any, @MessageBody() payload: any) {
+    async leaveGame(client: any, @MessageBody() payload: any) {
         this.gameResult = new gameResult('boo', false, 0, 0, 'You left the game')
         this.wss.emit('end-game', this.gameResult)
+        await this.addHistory()
     }
     @SubscribeMessage('move')
     movePlayer(client: any, @MessageBody() payload: any) {
