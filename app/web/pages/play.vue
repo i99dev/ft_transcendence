01308c7106099dc09a1e9bcefe91bf2e3351154b
BassamNaji <80152355+bassamnaji@users.noreply.github.com>
2023-03-25 12:31:46 +0400

    🎇 add pong game front-end
    * setup socket for project (#62)
    
    * pipe fixed
    
    * fixed
    
    * protected all functions
    
    * pipes with checks fixed
    
    * pipes with checks fixed
    
    * pipe a bit cleaner
    
    * formatted
    
    * bugs fixed
    
    * merging
    
    * fixed pack.json
    
    * handle basic of socket.io along with basic game elements
    
    * fix nuxt bug in docker container
    
    * set up basic structure for game socket on backend
    
    * handle signals for pid1 with init in docker compose
    
    * set up basic (temporary) game socket simulation
    
    * test jwt
    
    * set up socket connection
    
    * adding plugin for socket-io
    
    * update docker environment and use dockerfile for backend and frontend
    
    * fix fetching image in navbar, fix switching between pages in navbar by checking cookies only for now
    
    ---------
    
    Co-authored-by: Imad Saad <em4d2002@gmail.com>
    Co-authored-by: bassamnaji <bassam1881999@gmail.com>
    Co-authored-by: Imad Saad <96569765+isaad18@users.noreply.github.com>
    Co-authored-by: BassamNaji <80152355+bassamnaji@users.noreply.github.com>
    Co-authored-by: Saxsori <jungxbra@gmail.com>
    
    * make game responsive
    
    * Handle Responsive game completely
    
    * add init to front-end and back-end services
    
    * add: Created game dto
    
    * fix GameObjDto name + add gameID to PlayerDto
    
    * Added simple matchmaking + socket events
    
    * Formatting
    
    * changed some naming
    
    * setup primevue, update game to match flow requirements
    
    * connect clients with backend socket
    
    * add gamelogic, ball movement ,collison, scores
    
    * undo socket settings  cors + path
    
    * update drawing score
    
    * fix a minor bug in score drawing
    
    * some refactoring
    
    * set up game play button
    
    * handle exit game
    
    * edit some calculations
    
    * change speed
    
    * handle give up and game result
    
    * add winner manually
    
    * add buttons after game, update game results from client side
    
    * handle mouse movements
    
    * handle touch events
    
    * update responsitivity bugs
    
    * add blur effect to game result
    
    * activate Game-Over event
    
    * clean up the code, convert it to typescript
    
    * fix a bug with resize event, fix a bug for empty gameData
    
    ---------
    
    Co-authored-by: Obaid Al Teneiji <10709888+i99dev@users.noreply.github.com>
    Co-authored-by: Imad Saad <em4d2002@gmail.com>
    Co-authored-by: Imad Saad <96569765+isaad18@users.noreply.github.com>
    Co-authored-by: Saxsori <jungxbra@gmail.com>
    Co-authored-by: i99dev <obaid.altuniji@gmail.com>
    Co-authored-by: Moatasem <m3t9mm@gmail.com>

--- app/web/pages/play.vue
+++ app/web/pages/play.vue
@@ -1,9 +1,86 @@
 <template>
-  <GameBoard />
+    <div  v-if="!ready && firstGameReady" class="fixed inset-0 z-10 overflow-y-auto flex h-screen w-full justify-center items-center bg-slate-700">
+        <GameLoadingButton @StartGame="startGame" />
+    </div>
+    <div>
+        <GameClosePopup
+            v-if="exit"
+            @closePopup="switchExistStatus(false)"
+            @GiveUp="exitGame"
+            summary="Exit Game"
+            detail="You will be considered a LOSER since you give up in middle of the game!!"
+            confirmation="Are you sure you want to exit the game?"
+        />
+        <div class="container">
+            <Button @click="switchExistStatus(true)" icon="pi pi-times" />
+            <GameBoard @ReadyGame="setGameReady" @GameOver="gameOver($event)" ref="gameBoard" />
+        </div>
+        <GameResult v-if="gameResult" :gameResultMessage="gameResultMessage" @playAgain="playAgain"/>
+    </div>
 </template>
 
-<style>
-body {
-  background-color: #334155;
+<script lang="ts" setup>
+
+let exit = ref(false);
+let ready = ref(false)
+let firstGameReady = ref(true)
+let gameResult = ref(false)
+let gameResultMessage = ref("")
+let gameBoard = ref()
+
+const startGame = () : void => {
+    gameBoard.value.setup()
+    gameResult.value = false
 }
+
+const playAgain = () : void => {
+    gameBoard.value.setup()
+}
+
+const gameOver = (message: string) : void => {
+    gameBoard.value.destroy()
+    firstGameReady.value = false
+    ready.value = false
+    gameResult.value = true
+    gameResultMessage.value = message
+}
+        
+const setGameReady = () : void => {
+    ready.value = true
+    gameResult.value = false
+}
+
+const exitGame = () : void => {
+    gameBoard.value.giveUp()
+    gameBoard.value.destroy()
+    exit.value = false
+    ready.value = false
+    gameResult.value = false
+}
+
+const switchExistStatus = (status : boolean) : void => {
+    exit.value = status;
+}
+
+</script>
+
+<style>
+
+body {
+    background-color: #334155;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column; 
+    margin: 0vh;
+    padding: 0;
+    min-height: 100vh;
+    min-width: 100vw;
+    position: relative;
+    height: 100vh;
+}
+
 </style>
---
+++ app/web/plugins/primevue.js
@@ -0,0 +1,19 @@
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
+import SpeedDial from 'primevue/speeddial'
+import ConfirmPopup from 'primevue/confirmpopup';
+import ConfirmationService from 'primevue/confirmationservice';
+
+export default defineNuxtPlugin(nuxtApp => {
+    nuxtApp.vueApp.use(PrimeVue, { ripple: true })
+    nuxtApp.vueApp.use(ToastService)
+    nuxtApp.vueApp.use(ConfirmationService)
+    nuxtApp.vueApp.component('Button', Button)
+    nuxtApp.vueApp.component('InputText', InputText)
+    nuxtApp.vueApp.component('Toast', Toast)
+    nuxtApp.vueApp.component('SpeedDial', SpeedDial)
+    //other components that you need
+})
