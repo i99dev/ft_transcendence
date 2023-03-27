ee48e2da26491d0f8289db30b2c63c522ec77457
Imad Saad <em4d2002@gmail.com>
2023-03-27 22:56:20 +0400

    preparing format

--- app/api/src/module/chat/chat.controller.ts
+++ app/api/src/module/chat/chat.controller.ts
@@ -1,6 +1,14 @@
+import { Post } from '@nestjs/common'
 import { ChatService } from './chat.service'
+import { Controller } from '@nestjs/common'
 
 
+@Controller('/chat')
 export class ChatController {
     constructor(private readonly chatService: ChatService) {}
+
+    @Post('/room')
+    createRoom():boolean {
+        return true;
+    }
 }
--- app/api/src/module/chat/chat.service.ts
+++ app/api/src/module/chat/chat.service.ts
@@ -1,5 +1,7 @@
+import { PrismaService } from '@providers/prisma/prisma.service';
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class ChatService {
+    constructor(private prisma: PrismaService) {}
 }
