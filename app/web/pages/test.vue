f9409e84c8288b8918680c0b9d8940d6da23571d
Saxsori <jungxbra@gmail.com>
2023-03-29 03:51:47 +0400

    re-commit as the first failed

---
+++ app/web/pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+	  <button @click="getMatchHistory">Make Request</button>
+	</div>
+  </template>
+
+<script setup>
+
+const getMatchHistory = async () => {
+  const api = useRuntimeConfig().API_URL
+  const playerId = 'aaljaber' // Replace with actual player ID
+  const { data, error: errorRef } = await useFetch(`match-history/${playerId}`, {
+    method: 'GET',
+    baseURL: api,
+    headers: {
+      Authorization: `Bearer ${useCookie('access_token').value}`,
+    },
+  })
+  const error = errorRef.value
+  if (error) {
+    console.error('Failed to get match history:', error)
+  } else {
+    console.log('Match history:', data)
+  }
+}
+
+</script>
