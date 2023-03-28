c7db8d416f681a0e917127c9d9e84b4cba3cb354
Saxsori <jungxbra@gmail.com>
2023-03-28 10:38:35 +0400

    adding three possible endpoints

--- app/api/src/module/match-history/match-history.controller.ts
+++ app/api/src/module/match-history/match-history.controller.ts
@@ -2,4 +2,26 @@ import { Controller } from '@nestjs/common'
 import { Get, Param, Query } from '@nestjs/common'
 
 @Controller('match-history')
-export class MatchHistoryController {}
+export class MatchHistoryController {
+    @Get(':player') // /match-history/:playerId
+    getPlayerMatchHistory(@Param('player') player: string) {
+        return `Match history for ${player}`
+    }
+
+    @Get(':player') // /match-history/:playerId?winning=true&losing=false
+    getMatchHistoryByResult(
+        @Param('player') player: string,
+        @Query('winning') winning: boolean,
+        @Query('losing') losing: boolean,
+    ) {
+        return `Match history for ${player} with winning=${winning} and losing=${losing}`
+    }
+
+    @Get(':player') // /match-history/:playerId?sort=score_asc
+    getMatchHistoryBySort(
+        @Param('player') player: string,
+        @Query('sort') sort: 'score_asc' | 'score_des',
+    ) {
+        return `Match history for ${player} sorted by ${sort}`
+    }
+}
