5f2e6f60dce144cb7ed9ca5a189225c96ac54a52
Saxsori <jungxbra@gmail.com>
2023-03-30 07:40:34 +0400

    add more seeds

--- app/api/prisma/seeders/match-history/match-history.seeder.ts
+++ app/api/prisma/seeders/match-history/match-history.seeder.ts
@@ -66,10 +66,10 @@ export class MatchHistorySeeder {
             { username: 'oal-tena', score: 4, isWinner: false, gameId: gameid },
         ])
         gameid = '33s4'
-        await this.createMatch(gameid, new Date('2052-12-31T23:59:59.000Z'))
+        await this.createMatch(gameid, new Date('2052-01-31T23:59:59.000Z'))
         await this.assignOponents(gameid, [
             { username: 'aaljaber', score: 18, isWinner: true, gameId: gameid },
-            { username: 'mal-guna', score: 0, isWinner: false, gameId: gameid },
+            { username: 'mal-guna', score: 2, isWinner: false, gameId: gameid },
         ])
         gameid = '23se'
         await this.createMatch(gameid, new Date('2032-11-31T23:59:59.000Z'))
@@ -80,7 +80,25 @@ export class MatchHistorySeeder {
         gameid = 'dwsed'
         await this.createMatch(gameid, new Date('2022-12-31T23:59:59.000Z'))
         await this.assignOponents(gameid, [
-            { username: 'aaljaber', score: 11, isWinner: true, gameId: gameid },
+            { username: 'aaljaber', score: 21, isWinner: true, gameId: gameid },
+            { username: 'isaad', score: 0, isWinner: false, gameId: gameid },
+        ])
+        gameid = 'hhsj'
+        await this.createMatch(gameid, new Date('2024-11-31T23:59:59.000Z'))
+        await this.assignOponents(gameid, [
+            { username: 'mal-guna', score: 11, isWinner: true, gameId: gameid },
+            { username: 'isaad', score: 0, isWinner: false, gameId: gameid },
+        ])
+        gameid = 'jdl'
+        await this.createMatch(gameid, new Date('2012-12-30T23:59:59.000Z'))
+        await this.assignOponents(gameid, [
+            { username: 'mal-guna', score: 0, isWinner: false, gameId: gameid },
+            { username: 'oal-tena', score: 11, isWinner: true, gameId: gameid },
+        ])
+        gameid = 'sjhk'
+        await this.createMatch(gameid, new Date('2019-10-31T23:59:59.000Z'))
+        await this.assignOponents(gameid, [
+            { username: 'bnaji', score: 11, isWinner: true, gameId: gameid },
             { username: 'isaad', score: 0, isWinner: false, gameId: gameid },
         ])
     }
