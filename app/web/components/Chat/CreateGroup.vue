2416193d1412287916b72b03fa797dc05733ce5e
bassamnaji <bassam1881999@gmail.com>
2023-04-09 14:32:11 +0400

    update create group popup

--- app/web/components/Chat/CreateGroup.vue
+++ app/web/components/Chat/CreateGroup.vue
@@ -41,7 +41,7 @@
 
                     <form class="w-full max-w-sm">
                       <div class="flex items-center border-b border-indigo-500 py-2">
-                        <button class="bg-blue-100 p-2 rounded-full">
+                        <button class="bg-blue-100 p-2 rounded-full" type="button">
                           <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-camera-plus" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                             <path d="M12 20h-7a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v3.5"></path>
@@ -66,6 +66,8 @@
                           Cancel
                         </button>
                       </div>
+                      <!-- users -->
+                      <UserProfileList />
                     </form>
 
 
@@ -89,6 +91,5 @@ import {
 } from '@headlessui/vue'
 
 const {isOpen} = defineProps(['isOpen'])
-
 
 </script>
