d6b8187d2e1a30fb848dc05f7aa310d730f63b89
Imad Saad <em4d2002@gmail.com>
2023-04-11 17:24:00 +0400

    fixed

--- app/api/src/module/chat/repository/chat.repository.ts
+++ app/api/src/module/chat/repository/chat.repository.ts
@@ -8,7 +8,7 @@ export class ChatRepository {
     async sort(chatRooms: any[]) {
         for (let i: number = 0; i + 1 < chatRooms.length; i++) {
             let tmp;
-            if (chatRooms[i].chat_room.messages[0].created_at > chatRooms[i + 1].chat_room.messages[0].created_at) {
+            if (chatRooms[i].chat_room.messages[0].created_at < chatRooms[i + 1].chat_room.messages[0].created_at) {
                 tmp = chatRooms[i];
                 chatRooms[i] = chatRooms[i + 1];
                 chatRooms[i + 1] = tmp;
