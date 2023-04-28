b92d4e1328d1ab982668e5aa1282844b80b528da
Saxsori <jungxbra@gmail.com>
2023-04-28 14:04:35 +0400

    achievemnet front-end ui/ux done

--- app/web/components/Dash/Achievement.vue
+++ app/web/components/Dash/Achievement.vue
@@ -1,8 +1,16 @@
 <template>
 	<div>
-	    <div>Dash Achievement</div>
+        <div class="grid grid-cols-1 md:lg:xl:grid-cols-3 group bg-white shadow-xl shadow-neutral-100 border ">
+            <div
+                class="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
+                <span class="p-5 rounded-full bg-white text-white shadow-lg shadow-blue-800">
+					<img src="../../assets/png-clipart-monkey-d-luffy-gomu-gomu-no-mi-one-piece-devil-fruit-paramecia-all-kinds-of-fruit-purple-violet-removebg-preview.png" alt="achievement" />
+				</span>
+                <p class="text-xl font-medium text-slate-700 mt-3">Type</p>
+                <p class="mt-2 text-sm text-slate-500">Description</p>
+            </div>
+		</div>
 	</div>
-	
 </template>
 
 <script setup>
@@ -18,4 +26,5 @@ const handle_test = () => {
 
 </script>
 
-<style></style>
+<style>
+</style>
