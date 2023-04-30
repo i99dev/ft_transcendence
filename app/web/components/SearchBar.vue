ea4e85f5c7f0a7b8d894c481642a043803090928
Saxsori <jungxbra@gmail.com>
2023-04-30 13:20:32 +0400

    add navigate on click

--- app/web/components/SearchBar.vue
+++ app/web/components/SearchBar.vue
@@ -17,19 +17,22 @@
             class="bg-white w-full rounded-xl shadow-xl overflow-hidden p-1"
         >
             <!-- items -->
-            <div
-                v-for="user in users" :key="user" class="w-full flex p-3 pl-4 items-center hover:bg-gray-300 rounded-lg cursor-pointer"
-            >
-                <div class="mr-4">
-                    <div class="h-9 w-9 rounded-sm flex items-center justify-center text-3xl">
-                        <img :src="user.image" />
+            <div v-for="user in users" :key="user">
+                <div
+                    @click="$emit('userInput', user.username)"
+                    class="w-full flex p-3 pl-4 items-center hover:bg-gray-300 rounded-lg cursor-pointer"
+                >
+                    <div class="mr-4">
+                        <div class="h-9 w-9 rounded-sm flex items-center justify-center text-3xl">
+                            <img :src="user.image" />
+                        </div>
                     </div>
-                </div>
-                <div>
-                    <div class="font-bold text-lg">UserName: {{ user.username }} </div>
-                    <div class="text-xs text-gray-500">
-                        <span class="mr-2">Ladder: {{ user.ladder }} </span>
-                        <span class="mr-2">Name: {{ user.first_name }} </span>
+                    <div>
+                        <div class="font-bold text-lg">UserName: {{ user.username }}</div>
+                        <div class="text-xs text-gray-500">
+                            <span class="mr-2">Ladder: {{ user.ladder }} </span>
+                            <span class="mr-2">Name: {{ user.first_name }} </span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -64,8 +67,8 @@ const onInputChange = () => {
         console.log('debounce', searchInput.value)
         const data = await SearchUserNames(searchInput.value)
         console.log('data', data, data?.length)
-		showSuggestions.value = (data && data?.length > 0) ? true : false
-		users.value = data ? data : []
+        showSuggestions.value = data && data?.length > 0 ? true : false
+        users.value = data ? data : []
     }, 500)()
 }
 </script>
