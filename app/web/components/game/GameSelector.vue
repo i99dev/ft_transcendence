455e9ad622f7acfc7e95f54c695bef3baa123b73
Moatasem <m3t9mm@gmail.com>
2023-05-01 04:51:36 +0400

    fix some styling

--- app/web/components/game/GameSelector.vue
+++ app/web/components/game/GameSelector.vue
@@ -1,89 +1,92 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
-    <div class="bg-white p-6 rounded-lg shadow-lg relative w-full ">
-      <div class="aspect-w-3 aspect-h-4">
-        <div>
-          <div v-if="step === 1" key="step1">
-            <h2 class="text-2xl mb-4 text-center">Select Game Type</h2>
-            <ul class="space-y-2">
-              <li>
-                <button
-                  class="w-full py-2 px-4 border border-transparent rounded-md text-white bg-blue-500 hover:bg-blue-600"
-                  @click="selectGame('classic')">
-                  Classic Pong
-                </button>
-              </li>
-              <li>
-                <button
-                  class="w-full py-2 px-4 border border-transparent rounded-md text-white bg-blue-500 hover:bg-blue-600"
-                  @click="selectGame('custom')">
-                  Custom Pong
-                </button>
-              </li>
-            </ul>
-            <div class="mt-4 flex space-x-4 justify-center">
-              <button class="py-2 px-4 border border-transparent rounded-md text-white bg-red-500 hover:bg-red-600"
-                @click="goHome">
-                Home
-              </button>
-            </div>
-          </div>
-          <div v-if="step === 2" key="step2">
-            <h2 class="text-2xl mb-4 text-center">Select Game Mode</h2>
-            <ul class="space-y-2">
-              <li>
-                <button :class="selectedMode === 'single' ? 'bg-green-500' : 'bg-blue-500'"
-                  class="w-full py-2 px-4 border border-transparent rounded-md text-white hover:bg-blue-600"
-                  @click="selectMode('single')">
-                  Single Player
-                </button>
-              </li>
-              <li>
-                <button :class="selectedMode === 'multi' ? 'bg-green-500' : 'bg-blue-500'"
-                  class="w-full py-2 px-4 border border-transparent rounded-md text-white hover:bg-blue-600"
-                  @click="selectMode('multi')">
-                  Find Opponent
-                </button>
-              </li>
-            </ul>
-            <div v-if="selectedGame === 'custom'">
-              <h3 class="text-xl text-bold text-center mt-4 mb-2">Pick Two PowerUps</h3>
-              <h3 class="text-s text-blue-500 text-center  mb-2">Powerups can be used with buttons 1, 2</h3>
-              <div class="grid grid-cols-2 gap-2">
-                <div v-for="(powerup, index) in powerups" :key="index" class="flex items-center">
-                  <input type="checkbox" :id="powerup" :value="powerup" v-model="selectedPowerups"
-                    @change="checkPowerupLimit" class="form-checkbox text-blue-500" />
-                  <label :for="powerup" class="ml-2">{{ powerup }}</label>
-                </div>
-              </div>
-            </div>
-            <div class="mt-4 flex space-x-4 justify-center">
-              <button class="py-2 px-4 border border-transparent rounded-md text-white bg-blue-700 hover:bg-blue-800"
-                @click="step = 1">
-                Back
-              </button>
-              <button :disabled="!selectedMode || (selectedGame === 'custom' && selectedPowerups.length != 2)"
-                class="py-2 px-4 border border-transparent rounded-md text-white bg-blue-700 hover:bg-blue-800 disabled:opacity-50"
-                @click="emitGameSelected">
-                Start Game
-              </button>
-            </div>
-          </div>
-          <div v-else-if="step === 3" class="box">
-            <div class="loading-container flex flex-col items-center justify-center">
-              <div class="half-circle-spinner">
-                <div class="circle circle-1"></div>
-                <div class="circle circle-2"></div>
-              </div>
-              <p class="loading-text mt-2 text-lg font-bold">{{ loadingMsg }}</p>
-              <button v-if="selectedMode == 'multi'"
-                class="mt-4 py-2 px-4 border border-transparent rounded-md text-white bg-red-500 hover:bg-red-600"
-                @click="leaveQueue">
-                Leave Queue
-              </button>
-            </div>
+  <div
+    class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 bg-violet-900 bg-opacity-5 py-6 px-10 rounded-lg shadow-lg border border-violet-700 space-y-4">
+    <div v-if="step === 1" key="step1">
+      <h2 class="text-2xl text-center text-white font-bold mb-4">Select Game Type</h2>
+      <ul class="space-y-2">
+        <li>
+          <button
+            class="w-full py-2 px-4 rounded-md text-white bg-blue-500 bg-opacity-75 hover:bg-opacity-100 border-2 border-blue-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+            @click="selectGame('classic')">
+            Classic Pong
+          </button>
+        </li>
+        <li>
+          <button
+            class="w-full py-2 px-4 rounded-md text-white bg-blue-500 bg-opacity-75 hover:bg-opacity-100 border-2 border-blue-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+            @click="selectGame('custom')">
+            Custom Pong
+          </button>
+        </li>
+      </ul>
+      <div class="flex justify-center mt-4">
+        <button
+          class="py-2 px-4 rounded-md text-white bg-red-500 bg-opacity-75 hover:bg-opacity-100 border-2 border-red-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+          @click="goHome">
+          Home
+        </button>
+      </div>
+    </div>
+
+    <div v-if="step === 2" key="step2">
+      <h2 class="text-2xl text-center text-white font-bold mb-4">Select Game Mode</h2>
+      <ul class="space-y-2">
+        <li>
+          <button :class="selectedMode === 'single' ? 'bg-green-500' : 'bg-blue-500'"
+            class="w-full py-2 px-4 rounded-md text-white bg-opacity-75 hover:bg-opacity-100 border-2 border-blue-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+            @click="selectMode('single')">
+            Single Player
+          </button>
+        </li>
+        <li>
+          <button :class="selectedMode === 'multi' ? 'bg-green-500' : 'bg-blue-500'"
+            class="w-full py-2 px-4 rounded-md text-white bg-opacity-75 hover:bg-opacity-100 border-2 border-blue-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+            @click="selectMode('multi')">
+            Find Opponent
+          </button>
+
+
+        </li>
+      </ul>
+
+      <div v-if="selectedGame === 'custom'">
+        <h3 class="text-xl text-bold text-center text-white mt-4 mb-2">Pick Two PowerUps</h3>
+        <h3 class="text-s text-white text-center  mb-2">Powerups can be used with buttons 1, 2</h3>
+        <div class="grid grid-cols-2 gap-2 text-white">
+          <div v-for="(powerup, index) in powerups" :key="index" class="flex items-center">
+            <input type="checkbox" :id="powerup" :value="powerup" v-model="selectedPowerups" @change="checkPowerupLimit"
+              class="form-checkbox text-blue-500" />
+            <label :for="powerup" class="ml-2">{{ powerup }}</label>
           </div>
         </div>
+      </div>
+
+      <div class="mt-4 flex space-x-4 justify-center">
+        <button
+          class="py-2 px-4 border-2 border-blue-700 rounded-md text-white bg-blue-700 hover:bg-blue-800 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+          @click="step = 1">
+          Back
+        </button>
+        <button :disabled="!selectedMode || (selectedGame === 'custom' && selectedPowerups.length != 2)"
+          class="py-2 px-4 border-2 border-blue-700 rounded-md text-white bg-blue-700 hover:bg-blue-800 disabled:opacity-50 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+          @click="emitGameSelected">
+          Start Game
+        </button>
+      </div>
+    </div>
+
+    <div v-else-if="step === 3" class="box">
+      <div class="loading-container flex flex-col items-center justify-center">
+        <div class="half-circle-spinner">
+          <div class="circle circle-1"></div>
+          <div class="circle circle-2"></div>
+        </div>
+        <p class="loading-text mt-2 text-lg font-bold text-white">{{ loadingMsg }}</p>
+        <button v-if="selectedMode == 'multi'"
+          class="py-2 px-4 mt-4 rounded-md text-white bg-red-500 bg-opacity-75 hover:bg-opacity-100 border-2 border-red-500 transition-all duration-200 ease-in-out transform hover:scale-105 active:scale-95"
+          @click="leaveQueue">
+          Leave Queue
+        </button>
       </div>
     </div>
   </div>
@@ -190,5 +193,4 @@ const emitGameSelected = () => {
   100% {
     transform: rotate(360deg);
   }
-}
-</style>
+}</style>
