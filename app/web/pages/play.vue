ce48c74266669f2b18daf27954a065f5005f9b76
Saxsori <jungxbra@gmail.com>
2023-05-01 12:16:43 +0400

    add another image

--- app/web/pages/play.vue
+++ app/web/pages/play.vue
@@ -1,29 +1,57 @@
 <template>
     <div>
-        <img src="/audio.png" alt="Stop audio" @click="toggleAudio"
-            class="fixed top-4 right-4 cursor-pointer w-8 h-8 z-50" />
-        <div v-if="showSelector"
-            class="fixed inset-0 z-10 overflow-y-auto flex h-screen w-full justify-center items-center">
+        <img
+            src="/audio.png"
+            alt="Stop audio"
+            @click="toggleAudio"
+            class="fixed top-4 right-4 cursor-pointer w-8 h-8 z-50"
+        />
+        <div
+            v-if="showSelector"
+            class="fixed inset-0 z-10 overflow-y-auto flex h-screen w-full justify-center items-center"
+        >
             <div class="flex flex-col items-center">
-                <GameSelector @gameSelected="startGame" ref="gameSelector" @leaveQueue="leaveQueue" />
+                <GameSelector
+                    @gameSelected="startGame"
+                    ref="gameSelector"
+                    @leaveQueue="leaveQueue"
+                />
             </div>
         </div>
 
         <div>
-            <GameClosePopup v-if="exit" @closePopup="switchExistStatus(false)" @GiveUp="exitGame" summary="Exit Game"
+            <GameClosePopup
+                v-if="exit"
+                @closePopup="switchExistStatus(false)"
+                @GiveUp="exitGame"
+                summary="Exit Game"
                 detail="You will be considered a LOSER since you give up in middle of the game!!"
-                confirmation="Are you sure you want to exit the game?" />
+                confirmation="Are you sure you want to exit the game?"
+            />
             <div
-                class="container flex justify-center items-center flex-col m-0 p-0 min-h-screen min-w-screen relative h-screen">
+                class="container flex justify-center items-center flex-col m-0 p-0 min-h-screen min-w-screen relative h-screen"
+            >
                 <div class="relative w-full h-full">
-                    <GameBoard v-if="showBoard" @ReadyGame="setGameReady" @GameOver="gameOver($event)"
-                        @ExitBtn="switchExistStatus(true)" ref="gameBoard" />
+                    <GameBoard
+                        v-if="showBoard"
+                        @ReadyGame="setGameReady"
+                        @GameOver="gameOver($event)"
+                        @ExitBtn="switchExistStatus(true)"
+                        ref="gameBoard"
+                    />
                 </div>
             </div>
-            <GameResult v-if="gameResult" @vnode-mounted="exit = false" :gameResultMessage="gameResultMessage"
-                @playAgain="playAgain" />
+            <GameResult
+                v-if="gameResult"
+                @vnode-mounted="exit = false"
+                :gameResultMessage="gameResultMessage"
+                @playAgain="playAgain"
+            />
         </div>
-        <div v-if="showTab" class="fixed z-50 inset-0 bg-black bg-opacity-70 flex items-center justify-center">
+        <div
+            v-if="showTab"
+            class="fixed z-50 inset-0 bg-black bg-opacity-70 flex items-center justify-center"
+        >
             <div class="bg-white p-6 rounded-md text-center">
                 <h2 class="text-xl font-semibold mb-4">You can't use the app on multiple tabs</h2>
                 <p>Please use the other tab.</p>
@@ -33,11 +61,10 @@
 </template>
 
 <script lang="ts" setup>
-
 import { ref } from 'vue'
 import { useSocket, useTabEvent } from '@/composables/Game/useSocket'
 const emit = defineEmits(['showTabModal'])
-const exit = ref(false);
+const exit = ref(false)
 const showSelector = ref(true)
 const showBoard = ref(false)
 const gameResult = ref(false)
@@ -47,18 +74,23 @@ const gameSelector = ref()
 
 const { emitLeaveQueue } = useSocket()
 const { showTab } = useTabEvent()
-const audio = new Audio('/sounds/ost1.mp3');
-audio.loop = true;
-audio.volume = 0.1;
-
+const audio = new Audio('/sounds/ost1.mp3')
+audio.loop = true
+audio.volume = 0.1
 
 onMounted(() => {
-});
+    // audio.play().catch(err => {
+    //     console.log(err)
+    //     audio.pause()
+    //     document.addEventListener('click', function () {
+    //         audio.play()
+    //     })
+    // })
+})
 
 onBeforeUnmount(() => {
-    audio.pause();
-});
-
+    // audio.pause()
+})
 
 const startGame = (mode: GameSelectDto): void => {
     showBoard.value = true
@@ -70,9 +102,9 @@ const startGame = (mode: GameSelectDto): void => {
 }
 
 const playAgain = (): void => {
-    showSelector.value = true;
-    showBoard.value = false;
-    gameResult.value = false;
+    showSelector.value = true
+    showBoard.value = false
+    gameResult.value = false
 }
 
 const gameOver = (message: string): void => {
@@ -88,7 +120,6 @@ const leaveQueue = (): void => {
         showBoard.value = false
     }, 1000)
     console.log('leave queue !!')
-
 }
 
 const setGameReady = (): void => {
@@ -107,9 +138,9 @@ const exitGame = (): void => {
 const toggleAudio = (): void => {
     console.log('toggle audio')
     if (audio.paused) {
-        audio.play();
+        audio.play()
     } else {
-        audio.pause();
+        audio.pause()
     }
 }
 const switchExistStatus = (status: boolean): void => {
