739f86d72d595fe2dfe47c5e9a2559b8c7ba67d4
Imad Saad <em4d2002@gmail.com>
2023-05-15 21:12:26 +0400

    fixed

--- app/api/src/module/chat/chat.module.ts
+++ app/api/src/module/chat/chat.module.ts
@@ -2,13 +2,14 @@ import { PrismaService } from '../../providers/prisma/prisma.service'
 import { PrismaClient } from '@prisma/client'
 import { ChatService } from './chat.service'
 import { ChatController } from './chat.controller'
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { GroupChatService } from './groupChat.service'
 import { ChatRepository } from './repository/chat.repository'
 import { DirectChatService } from './directChat.service'
+import { ChatWsModule } from './gateway/chatWs.module'
 
 @Module({
-    imports: [],
+    imports: [forwardRef(() => ChatWsModule)],
     controllers: [ChatController],
     providers: [
         ChatService,
