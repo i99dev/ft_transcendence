bb9ca6aa43bb62d512ab3002317a01475243a8f6
Saxsori <saxxxsori@gmail.com>
2023-05-24 10:31:45 +0400

    fix audio toggle in play game

--- app/web/components/UserProfile/Control.vue
+++ app/web/components/UserProfile/Control.vue
@@ -56,7 +56,7 @@
                     d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286zm1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
             </svg>
         </button>
-        <button v-if="!muteSound" v-click-effect="handleMuteSound"
+        <button v-if="muteSound" v-click-effect="handleMuteSound"
             class="relative hover:bg-primary rounded-full smooth-transition p-2 w-12 aspect-square">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                 class="bi bi-volume-mute fill-white w-10 h-10" viewBox="0 0 16 16">
@@ -144,12 +144,10 @@ function openChatModel() {
 
 function handleMuteSound () {
     muteSound.value = !muteSound.value
-    console.log('mute', muteSound.value, 'pause', isPaused('login'))
-    if (isPaused('login') && muteSound.value)
+    if (isPaused('login'))
         play('login')
-    else if (!isPaused('login') && muteSound)
+    else if (!isPaused('login'))
         pause('login')
-
 }
 
 function goToHelp() {
@@ -179,3 +177,27 @@ const updateTwoFacAuth = async () => {
 
 
 </script>
+
+<!-- 
+<div>
+    <button v-if="muteSound" v-click-effect="handleMuteSound"
+        class="relative hover:bg-primary rounded-full smooth-transition p-2 w-12 aspect-square">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-volume-mute fill-white w-10 h-10" viewBox="0 0 16 16">
+            <path
+                d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zM6 5.04 4.312 6.39A.5.5 0 0 1 4 6.5H2v3h2a.5.5 0 0 1 .312.11L6 10.96V5.04zm7.854.606a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z" />
+        </svg>
+    </button>
+    <button v-else v-click-effect="handleMuteSound"
+        class="relative hover:bg-primary rounded-full smooth-transition p-2 w-12 aspect-square">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+            class="bi bi-volume-up fill-white w-10 h-10" viewBox="0 0 16 16">
+            <path
+                d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z" />
+            <path
+                d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z" />
+            <path
+                d="M10.025 8a4.486 4.486 0 0 1-1.318 3.182L8 10.475A3.489 3.489 0 0 0 9.025 8c0-.966-.392-1.841-1.025-2.475l.707-.707A4.486 4.486 0 0 1 10.025 8zM7 4a.5.5 0 0 0-.812-.39L3.825 5.5H1.5A.5.5 0 0 0 1 6v4a.5.5 0 0 0 .5.5h2.325l2.363 1.89A.5.5 0 0 0 7 12V4zM4.312 6.39 6 5.04v5.92L4.312 9.61A.5.5 0 0 0 4 9.5H2v-3h2a.5.5 0 0 0 .312-.11z" />
+        </svg>
+    </button>
+</div> -->
