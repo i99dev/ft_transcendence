9b472140649b863ccd7e58addd1607630f1c84d6
Saxsori <saxxxsori@gmail.com>
2023-05-26 23:06:08 +0400

    images only remains

--- app/web/pages/help.vue
+++ app/web/pages/help.vue
@@ -27,9 +27,13 @@
 
                         </div>
                         <div
-                            class="mt-12 flex transform items-center justify-center transition-transform duration-150 ease-in-out hover:scale-125">
-
-                            <img class="w-30 h-30" src="../assets/devilfruit.png" />
+                            class="mt-12 flex col-auto transform items-center justify-center transition-transform duration-150 ease-in-out hover:scale-125">
+                            <div v-if="ann.title == 'Controls'" class="flex flex-col items-center">
+                            <svg style="color: blueviolet" xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-arrow-up-square-fill mb-2" viewBox="0 0 16 16"> <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" fill="white"></path> </svg>
+                            <svg style="color: blueviolet" xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-arrow-down-square-fill" viewBox="0 0 16 16"> <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z" fill="white"></path> </svg>
+                            </div>
+                        </div>
+                        <div>
 
                         </div>
                     </div>
@@ -60,7 +64,7 @@ const announcement = computed(() => {
             title: 'Controls',
             subtitle: 'Use the following controls to play the game:',
             image: '../assets/devilfruit.png',
-            description: ['Arrow Buttons: Move the player paddle left and right.', '1 and 2 Buttons:: Activate power-ups.']
+            description: ['Arrow Buttons: Move the player paddle left and right.', '1 and 2 Buttons: Activate power-ups.']
         },
         {
             title: 'Rank System',
