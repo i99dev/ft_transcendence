ef6a1e7a9a3b8f864c17c2be655f14bdcfeaa484
bassamnaji <bassam1881999@gmail.com>
2023-05-26 23:17:41 +0400

    fix bugs

--- app/web/components/AnnouncePoPUp.vue
+++ app/web/components/AnnouncePoPUp.vue
@@ -1,39 +1,4 @@
 <template>
-    <div>
-        <!-- Announcement popup -->
-        <div v-if="isAnnounce">
-            <div v-for="(ann, index) in announcement" :key="index">
-                <div v-if="checkAnnounceAchiev(index)"
-                    class="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center">
-                    <div
-                        class="inline-block text-left bg-white rounded-lg overflow-hidden align-bottom transition-all transform shadow-2xl sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
-                        <div class="items-center w-full mr-auto ml-auto relative max-w-7xl md:px-12 lg:px-24">
-                            <div class="grid grid-cols-1">
-                                <div class="mt-4 mr-auto mb-4 ml-auto bg-white max-w-lg">
-                                    <div class="flex flex-col items-center pt-6 pr-6 pb-6 pl-6">
-                                        <img class="rounded-full" :src=getImagePath(ann.type) />
-                                        <p
-                                            class="mt-8 text-2xl font-semibold leading-none text-gray tracking-tighter lg:text-3xl">
-                                            {{ getAnnounceTitle(ann) }}
-                                        </p>
-                                        <p class="mt-3 text-base leading-relaxed text-center text-gray-600">
-                                            {{ getAnnounceContent(ann) }}
-                                        </p>
-                                        <div class="w-full mt-6">
-                                            <a v-click-effect="() => closeAcievPopUp(index)"
-                                                class="flex text-center items-center justify-center w-full pt-4 pr-10 pb-4 pl-10 text-base font-medium text-white bg-indigo-600 rounded-xl transition duration-500 ease-in-out transform hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                {{ getButtonName(ann) }} </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <div v-if="isAnnounce">
         <div v-for="(ann, index) in announcement" :key="index">
             <div v-if="checkAnnounceAchiev(index)"
@@ -44,7 +9,7 @@
                         <div
                             class="w-56 aspect-square rounded-full p-2 border border-tertiary hover:scale-105 smooth-transition"
                         >
-                            <img class="rounded-full"
+                            <img class="rounded-full object-cover"
                                 :src=getImagePath(ann.type)
                             />
                         </div>
